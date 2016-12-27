024011990606bb23cde30745a194d21c607708cf
大王 <1014114429@qq.com>
2016-12-27 21:44:43 +0800

    完善一部分焦点图

---
+++ duitang/main.js
@@ -0,0 +1,8 @@
+$('.Focus-point').click(function () {
+    $('.Focus-point').removeClass('is-current');
+    $(this).addClass('is-current');
+
+    var idx = $(this).index();
+    $('.Focus-item').removeClass('is-current');
+    $('.Focus-item').eq(idx).addClass('is-current');
+});
